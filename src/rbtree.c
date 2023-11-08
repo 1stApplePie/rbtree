3c685439d46012192ce8de8c5d34ac1680f26836
1stApplePie <chanwoo6422@naver.com>
2023-11-08 21:25:37 +0900

    last commit

--- src/rbtree.c
+++ src/rbtree.c
@@ -25,19 +25,32 @@
 
 typedef enum { ROTATE_RIGHT, ROTATE_LEFT } rotate_dir_t;
 
-void *bst_insert(rbtree *, node_t *);
+void *bstree_insert(rbtree *, node_t *);
 void *rbtree_insert_fixup(rbtree *, node_t *);
 void *rbtree_rotate(rbtree *, node_t *, const rotate_dir_t);
 void *rbtree_erase_fixup(rbtree *, node_t *);
 int rbtree_inorder(node_t *, key_t *, int);
 void *rbtree_transplant(rbtree *, node_t *, node_t *);
 
+/* 
+* @details Create a new red-black tree (rbtree) and initialize its properties.
+* @return A pointer to the newly created rbtree.
+*/
 rbtree *new_rbtree(void) {
+  /*
+  * void* calloc(size_t element_count, size_t element_size)
+    * element size 크기의 변수를 element count개 만큼 저장할 수 있는 메모리 공간을 할당
+  */
+
   // Dynamic alloc for tree
-  rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
+  // size of rbtree struct: 16 -> node_t pointer * 2
+  rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));  // 16바이트 크기의 변수 1개를 담을 수 있는 공간을 동적 할당
 
   // Dynamic alloc for node
-  node_t *NIL = (node_t *)calloc(1, sizeof(node_t));
+  // node_t: {color_t, key_t, struct node_t * 3}
+  // size of node_t: 32 -> color_t: 4, key_t: 4, pointer * 3: 24
+  node_t *NIL = (node_t *)calloc(1, sizeof(node_t));  // 32바이트 크기의 변수 1개를 담을 수 있는 공간을 동적 할당
+
   NIL->color = RBTREE_BLACK;
 
   p->nil = NIL;
@@ -45,6 +58,12 @@ rbtree *new_rbtree(void) {
   return p;
 }
 
+/* 
+* @details Inserts a new node with the specified key into the red-black tree (rbtree).
+* @param[in] t - A pointer to the rbtree.
+* @param key - The key to be inserted.
+* @return A pointer to the root of the modified rbtree.
+ */
 node_t *rbtree_insert(rbtree *t, const key_t key) {
   // create new node
   node_t *new_node = (node_t *)calloc(1, sizeof(node_t));
@@ -61,18 +80,29 @@ node_t *rbtree_insert(rbtree *t, const key_t key) {
     t->root = new_node;
   }
   else {
-    bst_insert(t, new_node);
+    bstree_insert(t, new_node);
     rbtree_insert_fixup(t, new_node);
   }
 
   return t->root;
 }
 
+/*
+ * @details Deallocates memory for the entire red-black tree (rbtree) structure.
+ * @param[in] t - A pointer to the rbtree to be deleted.
+ * @return void
+ */
 void delete_rbtree(rbtree *t) {
   // TODO: reclaim the tree nodes's memory
   free(t);
 }
 
+/*
+* @details Finds a node with the specified key in the red-black tree (rbtree).
+* @param[in] t - A pointer to the rbtree to search in.
+* @param[in] key - The key value to search for.
+* @return node_t - A pointer to the found node, or NULL if not found.
+*/
 node_t *rbtree_find(const rbtree *t, const key_t key) {
   node_t *current_node = t->root;
 
@@ -87,6 +117,11 @@ node_t *rbtree_find(const rbtree *t, const key_t key) {
   return NULL;
 }
 
+/*
+* @details Finds the node with the minimum key in the red-black tree (rbtree).
+* @param[in] t - A pointer to the rbtree to search in.
+* @return node_t - A pointer to the node with the minimum key.
+*/
 node_t *rbtree_min(const rbtree *t) {
   node_t *current_node = t->root;
 
@@ -96,6 +131,11 @@ node_t *rbtree_min(const rbtree *t) {
   return current_node;
 }
 
+/*
+* @details Finds the node with the maximum key in the red-black tree (rbtree).
+* @param[in] t - A pointer to the rbtree to search in.
+* @return node_t - A pointer to the node with the maximum key.
+*/
 node_t *rbtree_max(const rbtree *t) {
   node_t *current_node = t->root;
 
@@ -105,6 +145,12 @@ node_t *rbtree_max(const rbtree *t) {
   return current_node;
 }
 
+/*
+* @details Deletes a node with a given key from the red-black tree (rbtree).
+* @param[in] t - A pointer to the rbtree.
+* @param[in] p - A pointer to the node to delete.
+* @return int - Returns 0 on successful deletion.
+*/
 int rbtree_erase(rbtree *t, node_t *p) {
   node_t *delete_node = p;
   node_t *new_node;
@@ -151,13 +197,25 @@ int rbtree_erase(rbtree *t, node_t *p) {
   return 0;
 }
 
+/*
+ * @details Performs an inorder traversal of the rbtree and stores keys in an array.
+ * @param[in] t - A pointer to the rbtree.
+ * @param[out] arr - A pointer to the array to store the keys.
+ * @param[in] n - The maximum number of keys to store in the array.
+ * @return int - Returns 0 on successful traversal.
+ */
 int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
-  // TODO: implement to_array
   rbtree_inorder(t->root, arr, 0);
   return 0;
 }
 
-void *bst_insert(rbtree *t, node_t *new_node) {
+/* 
+* @details  Binary search tree insert function
+* @param[in]  rbtree_struct_pointer Red-Black Tree being inserted into.
+* @param[in]  node_t_struct_pointer Node to be inserted.
+* @return  void
+*/
+void *bstree_insert(rbtree *t, node_t *new_node) {
   // insert new node
   node_t *parent_node = t->root;
 
@@ -182,9 +240,14 @@ void *bst_insert(rbtree *t, node_t *new_node) {
   }
 }
 
+/* 
+* @details  Fixup Red Black Tree after binary search tree insert
+* @param[in]  rbtree_struct_pointer Red-Black Tree to Fixup.
+* @param[in]  node_t_struct_pointer Pivot node pointer.
+* @return  void
+*/
 void *rbtree_insert_fixup(rbtree *t, node_t *current_node) {
-  // A red node does not have a red child
-
+  // if parent node's color is red
   while (current_node->parent->color == RBTREE_RED){
     if (current_node->parent == current_node->parent->parent->left){  // case 1, 2, 3
       node_t *uncle_node = current_node->parent->parent->right;
@@ -238,6 +301,13 @@ void *rbtree_insert_fixup(rbtree *t, node_t *current_node) {
   t->root->color = RBTREE_BLACK;
 }
 
+/* 
+* @details  rotate 2 nodes - current node, child node, rotate direction - rotate_dir_t
+* @param[in]  rbtree_struct_pointer Red-Black Tree to Rotate.
+* @param[in]  node_t_struct_pointer Pivot node pointer.
+* @param[in]  rotate_direction left or right direction, rotate_dir_t = {ROTATE_LEFT, ROTATE_RIGHT}.
+* @return  void
+*/
 void *rbtree_rotate(rbtree *t, node_t *current_node, const rotate_dir_t rotate_dir) {
   if (rotate_dir == ROTATE_LEFT) {
     // set right node
@@ -254,23 +324,29 @@ void *rbtree_rotate(rbtree *t, node_t *current_node, const rotate_dir_t rotate_d
     if (current_node->parent == t->nil){
       t->root = right_node;
     }
-
     else if (current_node == current_node->parent->left){
       current_node->parent->left = right_node;
     }
     else {
       current_node->parent->right = right_node;
     }
+
+    // reconnect current node <-> right node
     right_node->left = current_node;
     current_node->parent = right_node;
   }
 
   else if (rotate_dir == ROTATE_RIGHT){
+    // set left node
     node_t *left_node = current_node->left;
+
+    // move left node's right subtree to current node's left subtree
     current_node->left = left_node->right;
     if (left_node->right != t->nil) {
       left_node->right->parent = current_node;
     }
+
+    // set left node's parent -> current node's parent
     left_node->parent = current_node->parent;
     if (current_node->parent == t->nil){
       t->root = left_node;
@@ -281,11 +357,19 @@ void *rbtree_rotate(rbtree *t, node_t *current_node, const rotate_dir_t rotate_d
     else {
       current_node->parent->left = left_node;
     }
+
+    // reconnect current node <-> right node
     left_node->right = current_node;
     current_node->parent = left_node;
   }
 }
 
+/*
+* @details Fixes the rbtree properties after erasing a node, preserving the rbtree structure.
+* @param[in] t - A pointer to the rbtree.
+* @param[in] p - A pointer to the node to fix the rbtree properties from.
+* @return void
+*/
 void *rbtree_erase_fixup(rbtree *t, node_t *p){
   while (p != t->root && p->color == RBTREE_BLACK){
     if (p == p->parent->left){
@@ -363,6 +447,13 @@ void *rbtree_erase_fixup(rbtree *t, node_t *p){
   p->color = RBTREE_BLACK;
 }
 
+/*
+* @details Replaces one subtree in the rbtree with another subtree.
+* @param[in] t - A pointer to the rbtree.
+* @param[in] delete_node - A pointer to the node to be replaced.
+* @param[in] new_node - A pointer to the new subtree.
+* @return void
+*/
 void *rbtree_transplant(rbtree *t, node_t *delete_node, node_t *new_node){
   if (delete_node->parent == t->nil){
     t->root = new_node;
@@ -377,6 +468,13 @@ void *rbtree_transplant(rbtree *t, node_t *delete_node, node_t *new_node){
   new_node->parent = delete_node->parent;
 }
 
+/*
+* @details Performs an inorder traversal of the rbtree and stores the keys in an array.
+* @param[in] root - A pointer to the root node of the subtree to traverse.
+* @param[out] arr - A pointer to the array for storing the keys.
+* @param[in] i - The current index in the array.
+* @return The updated index in the array after storing the keys.
+*/
 int rbtree_inorder(node_t *root, key_t *arr, int i) {
     if (root != NULL) {
         i = rbtree_inorder(root->left, arr, i);
